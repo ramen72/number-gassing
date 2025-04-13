25ffd1b2f5d0abbe8a64c2ecd6ab9d7191299950
Ramen Biswas <ramen@devsramen.com>
2025-04-13 22:43:21 +0600

    update Player-2 section at 13-04-2025_2243.

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -7,6 +7,9 @@ const HomeComponent = () => {
     const userInputValue = useSelector((state) => state.numberGassing.value)
     const player = useSelector((state) => state.numberGassing.player)
     const inValue = useSelector((state) => state.numberGassing.inValue)
+    const playerOneStatus = useSelector((state) => state.numberGassing.playerOneStatus)
+    const chance = useSelector((state) => state.numberGassing.chance)
+    const chanceStatus = useSelector((state) => state.numberGassing.chanceStatus)
 
     const dispatch = useDispatch()
 
@@ -16,9 +19,19 @@ const HomeComponent = () => {
                 <div className="w-[500px] h-96 bg-slate-400 border rounded-xl p-4 text-center">
                     <h1 className='font-bold text-4xl mb-6 text-white'>Number Gassing Game</h1>
                     <h3 className='mb-4 text-xl capitalize font-bold text-indigo-700'>{player}</h3>
+                    {
+                        chanceStatus && 
+                        <p className='mb-1 text-xl text-left capitalize text-indigo-700'>Chance : {chance}</p>
+                    }
                     <div className="flex justify-start items-center gap-x-1">
-                        <input value={inValue} onChange={(e) => dispatch(inputValue(e.target.value))} placeholder='Enter your number' className='p-2 w-[370px] border'/>
-                        <button onClick={(e)=>dispatch(playerOne())} className='w-24 py-2 px-3 border text-center bg-slate-400 transition-all duration-300 hover:bg-slate-500 hover:border-slate-400 capitalize font-bold hover:text-white'>Start</button>
+                        <input type='password' value={inValue} onChange={(e) => dispatch(inputValue(e.target.value))} placeholder='Enter your number' className='p-2 w-[370px] border'/>
+                        {
+                            playerOneStatus
+                            ?
+                            <button onClick={(e)=>dispatch(playerOne())} className='w-24 py-2 px-3 border text-center bg-slate-400 transition-all duration-300 hover:bg-slate-500 hover:border-slate-400 capitalize font-bold hover:text-white'>Start</button>
+                            :
+                            <button onClick={(e)=>dispatch(playerOne())} className='w-24 py-2 px-3 border text-center bg-slate-400 transition-all duration-300 hover:bg-slate-500 hover:border-slate-400 capitalize font-bold hover:text-white'>guess</button>
+                        }
                     </div>
                 </div>
             </div>
--- src/features/numberGassing/counterSlice.js
+++ src/features/numberGassing/counterSlice.js
@@ -5,6 +5,9 @@ const initialState = {
   inValue: "",
   player:"player-1",
   playerOneNumber:"",
+  chance:5,
+  chanceStatus:false,
+  playerOneStatus: true
 }
 
 export const numberGassingSlice = createSlice({
@@ -16,17 +19,24 @@ export const numberGassingSlice = createSlice({
     },
     playerOne: (state, action) => {
       if( state.inValue <= 0){
-        alert("t")
+        alert("Input Number can not be zero or negative value")
         state.inValue = ""
       }else{
         state.playerOneNumber = state.inValue
+        state.inValue = "";
+        state.player = "player-2";
+        state.playerOneStatus = false;
+        state.chanceStatus = true;
       }
       console.log(state.playerOneNumber)
+    },
+    playerTwo:(state, action)=>{
+
     }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { inputValue, playerOne, } = numberGassingSlice.actions
+export const { inputValue, playerOne, playerTwo, } = numberGassingSlice.actions
 
 export default numberGassingSlice.reducer
